e4f2c58917e6419acdd2d0c9e1424675f1007a4f
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 <nabag123@newschool.edu>
2023-05-04 18:55:51 -0400

    added all questions

--- script.js
+++ script.js
@@ -4,20 +4,125 @@ function showResult() {
     var resultMessage = "";
     var resultImage = "";
   
-    // Check answers for Question 1
+    // Check answers for QUESTION 1
     var q1 = document.quiz.q1.value;
-    if (q1 == "c") {
+
+    //If answer is Strawberry or Pineapple
+    if (q1 == "a" || q1 == "b") {
       score++;
     }
+
+    //If answer is Grape or Blueberry
+    if (q1 == "c" || q1 == "d") {
+
+    }
   
-    // Check answers for Question 2
+
+
+    // Check answers for QUESTION 2
     var q2 = document.quiz.q2.value;
-    if (q2 == "a") {
+
+    //If answer is Potato or Beans
+    if (q2 == "a" || q2 == "c") {
       score++;
     }
+
+    //If answer is Avocado or Tomato
+    if (q2 == "b" || q2 == "d") {
+
+    }
   
-    // Repeat for Questions 3-8
   
+
+    // Check answers for QUESTION 3
+    var q3 = document.quiz.q3.value;
+
+    //If answer is Cake or Ice Cream
+    if (q3 == "a" || q3 == "d") {
+      score++;
+    }
+
+    //If answer is Pastry or Cookie
+    if (q3 == "b" || q3 == "c") {
+
+    }
+
+
+
+    // Check answers for QUESTION 4
+    var q4 = document.quiz.q4.value;
+
+    //If answer is Pancakes/Waffles or Muffin
+    if (q4 == "a" || q4 == "c") {
+      score++;
+    }
+
+    //If answer is Omelet or Cereal
+    if (q4 == "b" || q4 == "d") {
+
+    }
+
+
+
+    // Check answers for QUESTION 5
+    var q5 = document.quiz.q5.value;
+
+    //If answer is Coffee or Tea
+    if (q5 == "a" || q5 == "c") {
+      score++;
+    }
+
+    //If answer is Water or Juice
+    if (q5 == "b" || q5 == "d") {
+
+    }
+
+
+
+    // Check answers for QUESTION 6
+    var q6 = document.quiz.q6.value;
+
+    //If answer is BBQ or Tacos
+    if (q6 == "b" || q6 == "c") {
+      score++;
+    }
+
+    //If answer is Sushi or Pizza
+    if (q6 == "a" || q6 == "d") {
+
+    }
+
+
+
+    // Check answers for QUESTION 7
+    var q7 = document.quiz.q7.value;
+
+    //If answer is Sweet or Sour
+    if (q7 == "a" || q7 == "b") {
+      score++;
+    }
+
+    //If answer is Savory or Spicy
+    if (q7 == "c" || q7 == "d") {
+
+    }
+
+
+
+    // Check answers for QUESTION 8
+    var q8 = document.quiz.q8.value;
+
+    //If answer is Chicken, Beef, or Pork
+    if (q8 == "a" || q8 == "b" || q8 == "c") {
+      score++;
+    }
+
+    //If answer is Other
+    if (q8 == "d") {
+
+    }
+
+
     // Calculate score percentage
     var scorePercentage = (score / totalQuestions) * 100;
   
